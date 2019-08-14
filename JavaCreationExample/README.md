e59770eaa36d93cb286e1fa2b27c326b58798fe2
Edwin Santos <edwsantos@gmail.com>
2019-08-13 20:15:45 -0500

    Creating a new simple annotation and use it on a class and method accordingly

---
+++ JavaCreationExample/README.md
@@ -0,0 +1,3 @@
+# Create new Java annotation
+In this module, we´re going to create a new simple annotation purely on java and then, we are going to use it in the 
+corresponding target (class or method) just for demonstrating how to create and use an annotation.  
---
+++ JavaCreationExample/src/main/java/com/infinity/annotation/definition/MyPersonalAnnotationForMethod.java
@@ -0,0 +1,12 @@
+package com.infinity.annotation.definition;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface MyPersonalAnnotationForMethod {
+    String value() default "Default method annotation description";
+}
